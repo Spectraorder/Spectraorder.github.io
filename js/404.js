f64cd63b38bc8d86d70c5e26f673d4f6e473b52b
Daniel <2421036333@qq.com>
2023-12-31 14:25:24 -0500

    404 choppy animation fix

--- js/404.js
+++ js/404.js
@@ -1,56 +1,36 @@
 const outerCircle = document.querySelectorAll('.circle');
 const innerCircle = document.querySelectorAll('.one');
-const orangeColumnOne = document.querySelectorAll('.two');
-const orangeColumnTwo = document.querySelectorAll('.three');
-const text = document.querySelectorAll('.p404');
+const orangeColumns = document.querySelectorAll('.two, .three');
+const textElements = document.querySelectorAll('.p404');
 
-const mousePos = {x:0, y:0};
+let mousePos = { x: 0, y: 0 };
 
-window.addEventListener("mousemove", function(e){
-    mousePos.x = e.clientX;
-    mousePos.y = e.clientY;
+function calculateOffsets(baseMultiplier) {
+  return {
+    horizontal: ((mousePos.x - window.innerWidth / 2) / window.innerWidth) * baseMultiplier,
+    vertical: ((mousePos.y - window.innerHeight / 2) / window.innerHeight) * baseMultiplier / 2,
+  };
+}
+
+function updateAnimation() {
+  const offsets = {
+    outerCircle: calculateOffsets(40),
+    innerCircle: calculateOffsets(30),
+    orangeColumns: calculateOffsets(20),
+    text: calculateOffsets(10),
+  };
+
+  outerCircle.forEach((element) => element.style.transform = `translate(${-offsets.outerCircle.horizontal}%, ${-offsets.outerCircle.vertical}%)`);
+  innerCircle.forEach((element) => element.style.transform = `translate(${-offsets.innerCircle.horizontal}%, ${-offsets.innerCircle.vertical}%)`);
+  orangeColumns.forEach((element) => element.style.transform = `translate(${-offsets.orangeColumns.horizontal}%, ${-offsets.orangeColumns.vertical}%)`);
+  textElements.forEach((element) => element.style.transform = `translate(${-offsets.text.horizontal}%, ${-offsets.text.vertical}%)`);
+
+  requestAnimationFrame(updateAnimation);
+}
+
+window.addEventListener("mousemove", (e) => {
+  mousePos.x = e.clientX;
+  mousePos.y = e.clientY;
 });
 
-function inverseMouseAnimation() {
-    let mouseX = mousePos.x;
-    let mouseY = mousePos.y;
-
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-
-    const outerCircleHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 40;
-    const outerCircleVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 20;
-
-    const innerCircleHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 30;
-    const innerCircleVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 15;
-
-    const orangeColumnHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 20;
-    const orangeColumnVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 10;
-
-    const textHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 10;
-    const textVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 5;
-
-    outerCircle.forEach((element) => {
-        element.style.transform = `translate(${-outerCircleHorizontalOffset}%, ${-outerCircleVerticalOffset}%)`;
-    });
-
-    innerCircle.forEach((element) => {
-        element.style.transform = `translate(${-innerCircleHorizontalOffset}%, ${-innerCircleVerticalOffset}%)`;
-    });
-
-    orangeColumnOne.forEach((element) => {
-        element.style.transform = `translate(${-orangeColumnHorizontalOffset}%, ${-orangeColumnVerticalOffset}%)`;
-    });
-
-    orangeColumnTwo.forEach((element) => {
-        element.style.transform = `translate(${-orangeColumnHorizontalOffset}%, ${-orangeColumnVerticalOffset}%)`;
-    });
-
-    text.forEach((element, index) => {
-        element.style.transform = `translate(${-textHorizontalOffset}%, ${-textVerticalOffset}%)`;
-    });
-    requestAnimationFrame(inverseMouseAnimation);
-}
-  
-requestAnimationFrame(inverseMouseAnimation);
-  
+requestAnimationFrame(updateAnimation);
