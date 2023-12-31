a9f00e379485faec992861976268c8d52743feec
Daniel <2421036333@qq.com>
2023-12-31 01:15:48 -0500

    404 animationframe fix

--- js/404.js
+++ js/404.js
@@ -4,44 +4,50 @@ const orangeColumnOne = document.querySelectorAll('.two');
 const orangeColumnTwo = document.querySelectorAll('.three');
 const text = document.querySelectorAll('.p404');
 
+const mousePos = {x:0, y:0};
+
+window.addEventListener("mousemove", function(e){
+    mousePos.x = e.clientX;
+    mousePos.y = e.clientY;
+});
+
 function inverseMouseAnimation() {
-    window.addEventListener('mousemove', (event) => {
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
-        const windowWidth = window.innerWidth;
-        const windowHeight = window.innerHeight;
-    
-        const outerCircleHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 40;
-        const outerCircleVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 20;
-    
-        const innerCircleHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 30;
-        const innerCircleVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 15;
+    let mouseX = mousePos.x;
+    let mouseY = mousePos.y;
 
-        const orangeColumnHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 20;
-        const orangeColumnVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 10;
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
 
-        const textHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 10;
-        const textVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 5;
-    
-        outerCircle.forEach((element) => {
-            element.style.transform = `translate(${-outerCircleHorizontalOffset}%, ${-outerCircleVerticalOffset}%)`;
-        });
-    
-        innerCircle.forEach((element) => {
-            element.style.transform = `translate(${-innerCircleHorizontalOffset}%, ${-innerCircleVerticalOffset}%)`;
-        });
+    const outerCircleHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 40;
+    const outerCircleVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 20;
 
-        orangeColumnOne.forEach((element) => {
-            element.style.transform = `translate(${-orangeColumnHorizontalOffset}%, ${-orangeColumnVerticalOffset}%)`;
-        });
+    const innerCircleHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 30;
+    const innerCircleVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 15;
 
-        orangeColumnTwo.forEach((element) => {
-            element.style.transform = `translate(${-orangeColumnHorizontalOffset}%, ${-orangeColumnVerticalOffset}%)`;
-        });
+    const orangeColumnHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 20;
+    const orangeColumnVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 10;
 
-        text.forEach((element, index) => {
-            element.style.transform = `translate(${-orangeColumnHorizontalOffset}%, ${-orangeColumnVerticalOffset}%)`;
-        });
+    const textHorizontalOffset = ((mouseX - windowWidth / 2) / windowWidth) * 10;
+    const textVerticalOffset = ((mouseY - windowHeight / 2) / windowHeight) * 5;
+
+    outerCircle.forEach((element) => {
+        element.style.transform = `translate(${-outerCircleHorizontalOffset}%, ${-outerCircleVerticalOffset}%)`;
+    });
+
+    innerCircle.forEach((element) => {
+        element.style.transform = `translate(${-innerCircleHorizontalOffset}%, ${-innerCircleVerticalOffset}%)`;
+    });
+
+    orangeColumnOne.forEach((element) => {
+        element.style.transform = `translate(${-orangeColumnHorizontalOffset}%, ${-orangeColumnVerticalOffset}%)`;
+    });
+
+    orangeColumnTwo.forEach((element) => {
+        element.style.transform = `translate(${-orangeColumnHorizontalOffset}%, ${-orangeColumnVerticalOffset}%)`;
+    });
+
+    text.forEach((element, index) => {
+        element.style.transform = `translate(${-textHorizontalOffset}%, ${-textVerticalOffset}%)`;
     });
     requestAnimationFrame(inverseMouseAnimation);
 }
