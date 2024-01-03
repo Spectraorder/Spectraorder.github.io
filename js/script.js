7f7c9a1f8a99abfce24fa572aaef12798791d3ed
Daniel <2421036333@qq.com>
2024-01-03 03:22:12 -0500

    first test slide-up-right keyframe

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ document.querySelector('.theme-switch__checkbox').addEventListener('change', fun
 const myName = document.querySelector('.my-name');
 function handleScroll() {
     const isVisible = window.scrollY >= myName.offsetTop - window.innerHeight;
-    console.log(isVisible);
-    console.log(window.scrollY);
     if(isVisible){
         myName.classList.add('animate');
     }
