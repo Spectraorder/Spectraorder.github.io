91427486bb24738d35764da7c5920485fe4d380d
Daniel <2421036333@qq.com>
2024-01-02 19:28:37 -0500

    my name animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 document.querySelector('.theme-switch__checkbox').addEventListener('change', function() {
     document.body.classList.toggle('dark-mode');
 });
+
+const myName = document.querySelector('.my-name');
+function handleScroll() {
+    const isVisible = window.scrollY >= myName.offsetTop - window.innerHeight;
+    console.log(isVisible);
+    console.log(window.scrollY);
+    if(isVisible){
+        myName.classList.add('animate');
+    }
+    else{
+        myName.classList.remove('animate');
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
