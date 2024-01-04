73ed6c75f6789b15eb3e3de891b3ff36e90faa6d
Daniel <2421036333@qq.com>
2024-01-04 01:12:43 -0500

    boarder radius for stroke | ease-in-out scrolling

--- js/lineScript.js
+++ js/lineScript.js
@@ -6,7 +6,11 @@ path.style.strokeDashoffset = pathLength;
 
 window.addEventListener('scroll', () =>{
     // var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-    var scrollPercentage = (window.scrollY - 300) / 1800;
+    const scrollPercentage = easeInOutQuad((window.scrollY - 300) / 1800);
     var drawLength = pathLength * scrollPercentage;
     path.style.strokeDashoffset = drawLength - pathLength;
 })
+
+function easeInOutQuad(t) {
+    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+}
