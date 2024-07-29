59457a322c1a2cedce99ce0ff7505dc7f208e3e4
Spectraorder <2421036333@qq.com>
2024-07-29 11:26:47 +0800

    remove 404 comment | scg line screen adjust

--- js/script.js
+++ js/script.js
@@ -15,6 +15,31 @@ function handleScroll() {
 
 window.addEventListener('scroll', handleScroll);
 
+function adjustSVG() {
+    const svgElement = document.querySelector('.line-container svg');
+    const windowWidth = window.innerWidth;
+
+    const minWidth = 2450;
+
+    const baseScale = 1;
+    const baseTranslateY = 100;
+
+    let scale = baseScale;
+    let translateY = baseTranslateY;
+
+    if (windowWidth > minWidth) {
+        const excessWidth = windowWidth - minWidth;
+        
+        scale = baseScale + (excessWidth / minWidth) * 2;
+        translateY = baseTranslateY + (excessWidth / 10);
+    }
+
+    svgElement.style.transform = `translateX(-250px) scale(${scale}) translateY(${translateY}px)`;
+}
+
+window.addEventListener('resize', adjustSVG);
+window.addEventListener('load', adjustSVG);
+
 if(window.innerWidth>767){
     window.addEventListener('scroll', () =>{
         const scrollLine  = document.querySelectorAll(".parallax");
